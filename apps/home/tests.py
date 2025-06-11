8f2a42b28a9dcfb1f3c7e3b6da218c388f1f1e87
alexfonfdz <fofapersonal@gmail.com>
2025-06-11 16:51:53 -0700

    Avance en el análisis ABC

--- apps/home/tests.py
+++ apps/home/tests.py
@@ -6,7 +6,7 @@ Copyright (c) 2019 - present AppSeed.us
 from asyncpg.connection import asyncio
 from decouple import os
 from django.test import TestCase
-from apps.home.product_abc_logic import refresh_data, calculate_product_abc
+from apps.home.product_abc_logic import refresh_data, calculate_product_abc, calculate_product_abc_todo, calculate_analysis_abc
 import aiomysql
 from core.settings import (
     ENV_MYSQL_USER,
@@ -48,11 +48,29 @@ class TestCalculateProductABC(TestCase):
             )
 
             await calculate_product_abc(
-                enterprise="MR DIESEL",
-                my_pool=my_pool
+                my_pool=my_pool,
+                enterprise_or_schema="MR DIESEL"
             )
 
         asyncio.run(run_test())
        
+class TestCalculateAnalysisABC(TestCase):
+    def test_a_calculate_analysis_abc(self):
 
+        async def run_test():
+            my_pool = await aiomysql.create_pool(
+            user=ENV_MYSQL_USER,
+            host=ENV_MYSQL_HOST,
+            port=int(ENV_MYSQL_PORT or 3306),
+            password=ENV_MYSQL_PASSWORD,
+            db=ENV_MYSQL_NAME,
+            minsize=3,
+            maxsize=5
+            )
 
+            await calculate_analysis_abc(
+                my_pool=my_pool,
+                enterprise_or_schema="MR DIESEL"
+            )
+
+        asyncio.run(run_test())
