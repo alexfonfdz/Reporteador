c52f7d6232440b2ec4701a74e7aab19c6965d9ed
alexfonfdz <fofapersonal@gmail.com>
2025-06-28 12:48:49 -0700

    Changes in change user password

--- apps/home/migrations/0017_change_user_password.py
+++ apps/home/migrations/0017_change_user_password.py
@@ -1,10 +1,11 @@
 from django.db import migrations
+from django.contrib.auth.hashers import make_password
 
 def change_admin_password(apps, schema_editor):
     User = apps.get_model('auth', 'User')
     try:
         admin = User.objects.get(username='admin')
-        admin.set_password('Secondsv1')
+        admin.password = make_password('admin')
         admin.save()
     except User.DoesNotExist:
         pass
